9ffaff788695d6b4d69e64aaa1cfc8b3dcc79991
Владимир Кирпичев <vkirpichev@inno.tech>
2025-03-02 17:04:52 +0300

    feat: Реализовал и типизировал функцию getData

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log("working ");
+import { getData } from "./task3";
+
+const COMMENTS_URL = "https://jsonplaceholder.typicode.com/comments";
+
+getData(COMMENTS_URL).then((data) => {
+  data.map((item) => console.log(`ID: ${item.id}, Email: ${item.email}`));
+});
---
+++ src/task3.ts
@@ -0,0 +1,25 @@
+//TODO Напишите и типизируйте функцию, выполняющую запрос за данными по переданному URL. Выведите их в консоль в формате: "ID: id, Email: email".
+
+export type CommentType = {
+  id: string;
+  email: string;
+  name?: string;
+  body?: string;
+};
+
+export const getData = (url: string): Promise<CommentType[]> => {
+  return fetch(url).then((response) => {
+    if (response.status !== 200) {
+      throw new Error("Что то пошло не так!!! ");
+    }
+    return response.json();
+  });
+};
+
+/**
+ * ID: 1, Email: Eliseo...
+ * ID: 2, Email: Jayne_Kuhic...
+ * ID: 3, Email: Nikita...
+ * ID: 4, Email: Lew...
+ * ...
+ */
